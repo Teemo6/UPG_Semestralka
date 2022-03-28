b4fd1cd11ae92844084c5095abe530152c4b32fb
Teemo6 <faragula.v@seznam.cz>
2022-03-28 23:18:26 +0200

    args[0]

--- src/Galaxy_SP2022.java
+++ src/Galaxy_SP2022.java
@@ -1,9 +1,11 @@
 public class Galaxy_SP2022 {
 
 	private static final String VSTUP_SOUBORU = "data/pulsar.csv";
-	private static final VstupDat VSTUP_DAT = new VstupDat(VSTUP_SOUBORU);
+	private static VstupDat VSTUP_DAT;
 
 	public static void main(String[] args) {
+		//VSTUP_DAT = new VstupDat(args[0]);
+		VSTUP_DAT = new VstupDat(VSTUP_SOUBORU);
 		System.out.println(VSTUP_DAT.getKonstantaG());
 		System.out.println(VSTUP_DAT.getCasovySkok());
 		for(Planeta p : VSTUP_DAT.getSeznamPlanet()){
